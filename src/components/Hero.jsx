14c09f7845f6c8c65b2ecb01c43a73daa830d5c8
Hoa JP <itlmh2k3@gmail.com>
2025-08-25 00:24:19 +0700

    update: new CV

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -76,7 +76,7 @@ const Hero = () => {
         {PROFILE.subheading}
       </p>
       <a
-        href="/public/public/Luong-Manh-Hoa-TopCVvn.pdf"
+        href="/public/public/Luong-Manh-Hoa-TopCV.vn-250825.02307.pdf"
         target="_blank"
         rel="nooperner noreferrer"
         download
